8735113dc2b4e179a9c02dc7efdc4431293ed05c
Dmitry Loparev <loparevdv@gmail.com>
2014-04-17 18:32:27 +0600

    issue #16 using COOKIES

--- source/tests/handlers.py
+++ source/tests/handlers.py
@@ -1,23 +1,48 @@
+import django.core.handlers.wsgi
+from django.conf import settings
+from django.utils.importlib import import_module
+session_engine = import_module(settings.SESSION_ENGINE)
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.wsgi
 import tornado.websocket
 
 import json
 
+from django.contrib.auth.models import User
 from source.tests.models import GlobalTesting
 from source.tests.classes import GTValues
 
-class GTValuesHandler(tornado.websocket.WebSocketHandler):
+class GTHandler(tornado.websocket.WebSocketHandler):
 
     def open(self, gt_id):
         # set self.gt_id for self.gtvalues
+
+        session_key = self.get_cookie(settings.SESSION_COOKIE_NAME)
+        session = session_engine.SessionStore(session_key)
+        try:
+            self.user_id = session["_auth_user_id"]
+            self.user = User.objects.get(id=self.user_id)
+        except (KeyError, User.DoesNotExist):
+            self.close()
+            return
+
         self.gt_id = gt_id
         scheduled = tornado.ioloop.PeriodicCallback(self.gtvalues, 500)
         scheduled.start()
 
     def on_message(self, message):
-        print 'message recieved {0}'.format(message)
+        tcigt_id = message.split('_')[-1]
+        if 'get_tcigt' in message:
+            print 'IM GOINT TO {0} WITH ID IS {1}'.format('get', tcigt_id)
+        if 'bugreport_tcigt' in message:
+            print 'IM GOINT TO {0} WITH ID IS {1}'.format('bugreport', tcigt_id)
+        if 'pass_tcigt' in message:
+            print 'IM GOINT TO {0} WITH ID IS {1}'.format('pass', tcigt_id)
+        if 'set_tcigt' in message:
+            print 'IM GOINT TO {0} WITH ID IS {1}'.format('set', tcigt_id)
 
     def on_close(self):
         print 'connection closed'
@@ -28,8 +53,12 @@ class GTValuesHandler(tornado.websocket.WebSocketHandler):
         gtvalues_dict = GTValues(gt).as_dict()
         self.write_message(json.dumps(gtvalues_dict))
 
+
+wsgi_app = tornado.wsgi.WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
 application = tornado.web.Application([
-    (r'/get_gtvalues/(?P<gt_id>\d+)/', GTValuesHandler),
+    (r'/get_gtvalues/(?P<gt_id>\d+)/', GTHandler),
+    (r'/pass_case_in_gt/(?P<tcigt_id>\d+)/', GTHandler),
+    ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
 ])
 
 if __name__ == '__main__':
